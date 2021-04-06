98f2b8fb2b625995057c7635a25bd3ba5d201c04
WeiningLi <bill.weining.li@gmail.com>
2021-04-05 21:05:29 -0400

    update icon & navbar quick fix

--- lp-forum/src/components/header.js
+++ lp-forum/src/components/header.js
@@ -5,60 +5,69 @@ import { GithubOutlined } from "@ant-design/icons";
 
 function Header() {
   return (
-      <div className="hero-head">
-        <nav className="navbar lp-navbar" role="navigation">
-          <div className="container">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="https://layout-parser.github.io/">
-                <img src={image} alt="Logo" className="lp-badge"></img>
-              </a>
+    <div className="hero-head">
+      <nav className="navbar lp-navbar" role="navigation">
+        <div className="container">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://layout-parser.github.io/">
+              <img src={image} alt="Logo" className="lp-badge"></img>
+            </a>
 
-              <div
-                className="navbar-burger"
-                data-target="navbar-main"
-                onClick={function () {
-                  document
-                    .querySelector(".navbar-menu")
-                    .classList.toggle("is-active");
-                }}
-              >
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
+            <div
+              className="navbar-burger"
+              data-target="navbar-main"
+              onClick={function () {
+                document
+                  .querySelector(".navbar-menu")
+                  .classList.toggle("is-active");
+              }}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
+          </div>
 
-            <div className="navbar-menu" id="navbar-main">
-              <div className="navbar-end">
-                <a href="https://layout-parser.github.io/tutorials.html" className="navbar-item">
-                  Tutorials
-                </a>
+          <div className="navbar-menu" id="navbar-main">
+            <div className="navbar-end">
+              <a
+                href="https://layout-parser.github.io/tutorials.html"
+                onMouseDown={(e) => {e.preventDefault();}}
+                className="navbar-item"
+              >
+                Tutorials
+              </a>
+              <a
+                href="https://layout-parser.readthedocs.io/en/latest/"
+                rel="noreferrer"
+                target="_blank"
+                onMouseDown={(e) => {e.preventDefault();}}
+                className="navbar-item"
+              >
+                Docs
+              </a>
+              <a
+                href="https://join.slack.com/t/layout-parser/shared_invite/zt-ohjd14k1-OrJ2HltwVRGrxhLeHMfW_w"
+                onMouseDown={(e) => {e.preventDefault();}}
+                className="navbar-item"
+              >
+                Discussion
+              </a>
+              <div className="navbar-item">
                 <a
-                  href="https://layout-parser.readthedocs.io/en/latest/"
+                  href="https://github.com/Layout-Parser/layout-parser"
                   rel="noreferrer"
                   target="_blank"
-                  className="navbar-item"
+                  className="icon bg-transparent button is-danger"
                 >
-                  Docs
+                  <GithubOutlined />
                 </a>
-                <a href="https://join.slack.com/t/layout-parser/shared_invite/zt-ohjd14k1-OrJ2HltwVRGrxhLeHMfW_w" className="navbar-item">
-                  Discussion
-                </a>
-                <div className="navbar-item">
-                  <a
-                    href="https://github.com/Layout-Parser/layout-parser"
-                    rel="noreferrer"
-                    target="_blank"
-                    className="icon bg-transparent button is-danger"
-                  >
-                    <GithubOutlined />
-                  </a>
-                </div>
               </div>
             </div>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
